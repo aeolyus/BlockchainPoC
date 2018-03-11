444c45a40c43e610cae77682c8c3aaab0010487a
Richard Huang <richardhuang.huang@gmail.com>
2018-03-11 10:48:20 -0700

    transaction generate and verify sig

--- src/Transaction.java
+++ src/Transaction.java
@@ -1,3 +1,4 @@
+import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.util.ArrayList;
 
@@ -29,5 +30,15 @@ public class Transaction {
 		sequence++;// increase sequence to avoid two identical transactions having the same hash
 		return StringUtil.applySha256(StringUtil.getStringFromKey(sender) + StringUtil.getStringFromKey(recipient)
 				+ Float.toString(value) + sequence);
+	}
+	
+	public void generateSignature(PrivateKey privateKey) {
+		String data = StringUtil.getStringFromKey(sender)+StringUtil.getStringFromKey(recipient)+Float.toString(value);
+		signature=StringUtil.applyECDSASig(privateKey, data);
+	}
+	
+	public boolean verifySignature() {
+		String data = StringUtil.getStringFromKey(sender)+StringUtil.getStringFromKey(recipient)+Float.toString(value);
+		return StringUtil.verifyECDSASig(sender, data, signature);
 	}
 }
