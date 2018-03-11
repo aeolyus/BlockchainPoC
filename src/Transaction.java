db843a02816033b0beb9d924d6445c52a45d7309
Richard Huang <richardhuang.huang@gmail.com>
2018-03-11 09:19:55 -0700

    transaction

---
+++ src/Transaction.java
@@ -0,0 +1,32 @@
+import java.security.PublicKey;
+import java.util.ArrayList;
+
+/**
+ * @author aeolyus
+ *
+ */
+public class Transaction {
+
+	public String transactionId;// hash of transaction
+	public PublicKey sender;
+	public PublicKey recipient;
+	public float value;
+	public byte[] signature;
+	
+	public ArrayList<TransactionInput> inputs = new ArrayList<TransactionInput>();
+	public ArrayList<TransactionOutput> outputs = new ArrayList<TransactionOutput>();
+	
+	private static int sequence=0;//count of how many transactions have been generated
+	
+	public Transaction(PublicKey from, PublicKey to, float value, ArrayList<TransactionInput> inputs) {
+		this.sender=from;
+		this.recipient=to;
+		this.value=value;
+		this.inputs=inputs;
+	}
+	
+	private String calculateHash() {
+		sequence++;//increase sequence to avoid two identical transactions having the same hash
+		return StringUtil.applySha256(StringUtil.getStringFromKey(sender)+StringUtil.getStringFromKey(recipient)+Float.toString(value)+sequence);
+	}
+}
--- src/Wallet.java
+++ src/Wallet.java
@@ -4,6 +4,7 @@ import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.SecureRandom;
 import java.security.spec.ECGenParameterSpec;
+
 /**
  * @author aeolyus
  *
@@ -12,23 +13,24 @@ public class Wallet {
 
 	public PrivateKey privateKey;
 	public PublicKey publicKey;
-	
-	public Wallet(){
+
+	public Wallet() {
 		generateKeyPair();
 	}
-	
+
 	public void generateKeyPair() {
 		try {
-			KeyPairGenerator keyGen=KeyPairGenerator.getInstance("ECDSA","BC");//elliptic curve digital signature algorithm, bouncy castle
-			SecureRandom random=SecureRandom.getInstance("SHA1PRNG");//faster than NativePRNG
+			KeyPairGenerator keyGen = KeyPairGenerator.getInstance("ECDSA", "BC");// elliptic curve digital signature
+																					// algorithm, bouncy castle
+			SecureRandom random = SecureRandom.getInstance("SHA1PRNG");// faster than NativePRNG
 			ECGenParameterSpec ecSpec = new ECGenParameterSpec("prime192v1");
-			
-			keyGen.initialize(ecSpec,random);//256 bytes
-			KeyPair keyPair=keyGen.generateKeyPair();
-			
-			privateKey=keyPair.getPrivate();
-			publicKey=keyPair.getPublic();
-		}catch(Exception e) {
+
+			keyGen.initialize(ecSpec, random);// 256 bytes
+			KeyPair keyPair = keyGen.generateKeyPair();
+
+			privateKey = keyPair.getPrivate();
+			publicKey = keyPair.getPublic();
+		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
 	}
