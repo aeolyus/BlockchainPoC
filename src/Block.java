b4b7c5b832195b82f3fec9087064274409ebfcdc
Richard Huang <richardhuang.huang@gmail.com>
2018-03-13 00:03:27 -0700

    implement merkleroot in block

--- src/Block.java
+++ src/Block.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Date;
 
 /**
@@ -6,7 +7,8 @@ import java.util.Date;
  */
 public class Block{
 
-	public String hash,previousHash;
+	public String hash,previousHash,merkleRoot;
+	public ArrayList<Transaction> transactions=new ArrayList<Transaction>();
 	private String data;
 	private long timeStamp;//milliseconds since 1/1/1970
 	private int nonce;
@@ -19,11 +21,12 @@ public class Block{
 	}
 
 	public String calculateHash(){
-		String calculatedhash=StringUtil.applySha256(previousHash+Long.toString(timeStamp)+Integer.toString(nonce)+data);
+		String calculatedhash=StringUtil.applySha256(previousHash+Long.toString(timeStamp)+Integer.toString(nonce)+merkleRoot+data);
 		return calculatedhash;
 	}
 
 	public void mineBlock(int difficulty){
+		merkleRoot=StringUtil.getMerkleRoot(transactions);
 		String target=new String(new char[difficulty]).replace('\0','0');
 		while(!hash.substring(0, difficulty).equals(target)){
 			nonce++;
