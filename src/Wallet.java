2c9a9e3183446b61a748423f43e1461dfb8491a4
Richard Huang <richardhuang.huang@gmail.com>
2018-03-12 23:56:32 -0700

    wallet getbalance and sendfunds

--- src/Wallet.java
+++ src/Wallet.java
@@ -4,6 +4,9 @@ import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.SecureRandom;
 import java.security.spec.ECGenParameterSpec;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author aeolyus
@@ -13,6 +16,8 @@ public class Wallet{
 
 	public PrivateKey privateKey;
 	public PublicKey publicKey;
+	
+	public HashMap<String,TransactionOutput>UTXOs=new HashMap<String,TransactionOutput>();//only UTXOs owned by this wallet
 
 	public Wallet(){
 		generateKeyPair();
@@ -33,5 +38,39 @@ public class Wallet{
 		}catch(Exception e){
 			throw new RuntimeException(e);
 		}
+	}
+	
+	public float getBalance(){
+		float total=0;
+		for(Map.Entry<String,TransactionOutput> item:Chain.UTXOs.entrySet()){
+			TransactionOutput UTXO=item.getValue();
+			if(UTXO.isMine(publicKey)){
+				UTXOs.put(UTXO.id, UTXO);//add to list of unspent transactions
+				total+=UTXO.value;
+			}
+		}
+		return total;
+	}
+	
+	public Transaction sendFunds(PublicKey _recipient,float value){
+		if(getBalance()<value){
+			System.out.println("#Not enough funds to send transaction. Transaction discarded.");
+			return null;
+		}
+		ArrayList<TransactionInput> inputs=new ArrayList<TransactionInput>();
+		float total=0;
+		for(Map.Entry<String,TransactionOutput> item:UTXOs.entrySet()){
+			TransactionOutput UTXO=item.getValue();
+			total+=UTXO.value;
+			inputs.add(new TransactionInput(UTXO.id));
+			if(total>value)
+				break;
+		}
+		Transaction newTransaction=new Transaction(publicKey,_recipient,value,inputs);
+		newTransaction.generateSignature(privateKey);
+		for(TransactionInput input:inputs){
+			UTXOs.remove(input.transactionOutputId);
+		}
+		return newTransaction;
 	}
 }
